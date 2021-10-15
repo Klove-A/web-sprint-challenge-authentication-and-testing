ee602e7c55d2daeda2ce19e494a762e4042e49c5
Klove Adams <kloveadams@gmail.com>
2021-10-15 16:04:37 -0400

    wrote first test

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,29 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+const request = require("supertest");
+const server = require("./server");
+const db = require("../data/dbConfig");
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+afterAll(async () => {
+  await db.destroy();
+});
+
+test("sanity", () => {
+  expect(true).toBe(true);
+});
+
+describe("[POST] /api/auth/register", () => {
+  let res;
+  beforeEach(async () => {
+    res = await request(server)
+      .post("/api/auth/register")
+      .send({ username: "greg", password: "1234" });
+  });
+  test("[1]registering user increase his legs of users table", async () => {
+    const users = await db("users")
+    expect(users).toHaveLength(1)
+  })
+});
